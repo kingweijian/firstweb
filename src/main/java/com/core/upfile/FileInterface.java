066b40b7dab6d9d4a2686ae6d3b57b5285cee441
weijian <wj97103@outlook.com>
2019-11-24 23:47:42 +0800

    修改架构

--- src/main/java/com/core/upfile/FileInterface.java
+++ src/main/java/com/core/upfile/FileInterface.java
@@ -1,7 +1,10 @@
 package com.core.upfile;
 
+import java.io.IOException;
+
 public interface FileInterface {
     public void setMaxSize(int size);
     public void setSuffixlimit(String[] suffixlimit);
     public void setSavePath(String path);
+    public boolean move_uploaded(byte[] filecontent,String filename,String filepath) throws IOException;
 }
